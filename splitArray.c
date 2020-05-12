8bf681de73200ce87884c4f81c7dfceb536ab146
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-05-12 17:07:45 +0200

    Update splitArray.c

--- splitArray.c
+++ splitArray.c
@@ -1,27 +1,33 @@
-// Compiled with: gcc -std=c11 -Wall -pthread -lm
-
 #include <stdio.h>
+#include <string.h>
 
-void splitArray(char *part, int numbers[], int num, int numParts, int index) {
+void splitArray(int *parts, int numbers[], int num, int numParts, int index) {    
     for(int i = 0; i < num; ++i)
-        part[i] = numbers[i];
+        parts[i] = numbers[i];
 }
 
 
 int main(void)
 {
+    printf("Hello, ");
     
+    //VARIABLES
+    int numParts = 2;     //How many parts we want
+    int index = 0;        //What part we want
+    
+    //INPUT
     int numbers[] = { 1, 2, 3, 4, 5, 6 };
     int numbersSize = sizeof numbers / sizeof numbers[0];
 
-    printf("%i, ", numbersSize);
+    //OUTPUT
+    const int partsSize = numbersSize / numParts;
+    int parts[partsSize];
+    memset(parts, 0, sizeof parts);    //Initialize array with 0s
     
-    char part[3] = {0};
-    splitArray(part, numbers, 3, 2, 0);
+    splitArray(parts, numbers, numbersSize, numParts, index);
     
-
-    for (int i=0; i<3; i++){
-        printf("%i, ", part[i]);
+    for (int i=0; i<partsSize; i++){
+        printf("%i, ", parts[i]);
     }
     
     
