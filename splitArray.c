39aa745e0698226bd96c3f91b45026ea4446fdbf
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-05-12 17:41:06 +0200

    Update splitArray.c

--- splitArray.c
+++ splitArray.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-void splitArray(int *parts, int numbers[], int num, int numParts, int index) {    
-    for(int i = 0; i < num; ++i)
-        parts[i] = numbers[i];
+void splitArray(int *parts, int numbers[], int numbersSize, int numParts, int index) {
+    
+    int partsSize = numbersSize / numParts;
+    
+    int start = index * partsSize;
+    int end = start + partsSize;
+     
+    if( index+1 == numParts ){     //If its the last index add the rest of the numbers
+        end = numbersSize;
+    }
+    
+    for(int i = start; i <= end; ++i){
+        parts[i-start] = numbers[i];
+    }
 }
 
 
 int main(void)
 {
-    printf("Hello: ");
     
     //VARIABLES
-    int numParts = 1;     //How many parts we want
-    int index = 0;        //What part we want
+    int numParts = 4;     //How many parts we want
+    int index = 3;        //What part we want
     
     //INPUT
     int numbers[] = { 1, 2, 3, 4, 5, 6 };
     int numbersSize = sizeof numbers / sizeof numbers[0];
 
     //OUTPUT
-    const int partsSize = numbersSize / numParts;
+    int partsSize = numbersSize / numParts;
     int parts[partsSize];
     memset(parts, 0, sizeof parts);    //Initialize array with 0s
     
@@ -28,6 +38,11 @@ int main(void)
     splitArray(parts, numbers, numbersSize, numParts, index);
     
     //SHOW RESULT
+    
+    if( index+1 == numParts ){         //If its the last index print the rest of the numbers
+        partsSize = (numbersSize % numParts) +1;
+    }
+    
     for (int i=0; i<partsSize; i++){
         printf("%i, ", parts[i]);
     }
