1449a9dfdf78a506e4e91c38bae8ada26faade3c
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-05-17 11:44:13 +0200

    Update mpi.c

--- mpi.c
+++ mpi.c
@@ -44,7 +44,7 @@ int removeNonPrimes(int *primes, int numbers[], int numbersSize) {
   return primesSize;
 }
 
-int largest(int arr[], int n) {
+int largestNumber(int arr[], int n) {
   int max = arr[0];
 
   for (int i = 1; i < n; i++) {
@@ -74,15 +74,15 @@ int main(int argc, char* argv[]) {
 
   if(mpi_comm_rank == 0){
 
+    // INPUT
+    int numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+                       13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                       26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                       39, 40, 41, 42, 43, 44, 45, 46, 47};
+
     for (int i = 1; i < mpi_comm_size; i++) {
       // VARIABLES
       int index = i;    // What part we want
-
-      // INPUT
-      int numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
-                         13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                         26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-                         39, 40, 41, 42, 43, 44, 45, 46, 47};
 
       int numbersSize = sizeof numbers / sizeof numbers[0];
 
@@ -100,25 +100,34 @@ int main(int argc, char* argv[]) {
       MPI_Send(&numeroSplit, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
       MPI_Send(&parts, numeroSplit, MPI_INT, i, 1, MPI_COMM_WORLD);
 
+    }
 
-      //printf("Index: %i:  %i # ", index, numeroSplit);
-      //printArray(parts, numeroSplit);
+    for (int i=1; i<mpi_comm_size; i++){
+      int largest = 0;
+      //printf("DEBUG: Master empieza a recibir de %i ", i);
+      MPI_Recv(&largest, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+      //printf("DEBUG: Master acaba de recibir de %i ", i);
+      printf("Message received from %i: Largest is %i \n", i, largest);
     }
 
   }else{
     
-    
+    // RECEIVING WORK    
     int numeroSplit = 0;
-
     MPI_Recv(&numeroSplit, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-
     int parts[numeroSplit];
-
     MPI_Recv(&parts, numeroSplit, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
 
-    //printf("Message received: %i - Message at  \n", numeroSplit);
+    // REMOVING NON PRIMES
+    int primes[10];
+    int primesSize = removeNonPrimes(primes, parts, numeroSplit);
 
-    printArray(parts, numeroSplit);
+    // SENDING LARGEST PRIME
+    int largest = largestNumber(primes, primesSize);
+    //printf("DEBUG: El worker %i hace send ", mpi_comm_rank);
+    MPI_Send(&largest, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    //printf("DEBUG: El worker %i acaba de hacer send ", mpi_comm_rank);
+
 
   }
 
