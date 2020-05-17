249a27edc34febd8bb6432a553b7bed6c1d6c251
isaMe98 <65238815+isaMe98@users.noreply.github.com>
2020-05-17 10:52:18 +0200

    Update mpi.c

--- mpi.c
+++ mpi.c
@@ -70,27 +70,33 @@ int main(int argc, char* argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_comm_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_comm_rank);
 
+  int index;
+  int numbers[];
   int numeroSplit;
+  int numbersSize;
+  int numParts;
+  int partsSize;
+  int parts[];
 
   if(mpi_comm_rank == 0){
 
     for (int i = 1; i < mpi_comm_size; i++) {
       // VARIABLES
-      int index = i;    // What part we want
+      index = i;    // What part we want
 
       // INPUT
-      int numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+      numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
                          13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
                          26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
                          39, 40, 41, 42, 43, 44, 45, 46, 47};
 
-      int numbersSize = sizeof numbers / sizeof numbers[0];
+      numbersSize = sizeof numbers / sizeof numbers[0];
 
-      int numParts = mpi_comm_size;
+      numParts = mpi_comm_size;
 
       // OUTPUT
-      int partsSize = numbersSize / numParts;
-      int parts[partsSize];
+      partsSize = numbersSize / numParts;
+      parts[partsSize];
       memset(parts, 0, sizeof parts); // Initialize array with 0s
 
       // FUNCTION
