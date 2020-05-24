d54aa19a0c58a1908683bc9926be8065c2166a27
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-05-24 12:58:10 +0200

    Working version

--- Test2_OpenCL/main.cpp
+++ Test2_OpenCL/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
 #ifdef __APPLE__
-
 #include <OpenCL/opencl.h>
-
 #else
+
 #include <CL/cl.h>
+
 #endif
 
-char* getKernelCode(const char* fileName, size_t *kernelSourceSize);
+char *getKernelCode(const char *fileName, size_t *kernelSourceSize);
+
 int getStart(int numbersSize, int numParts, int index);
+
 int getEnd(int numbersSize, int numParts, int index);
+
+int largestNumber(int arr[], int n);
 
 int main() {
 
@@ -21,12 +25,12 @@ int main() {
     int start[numParts];
     int end[numParts];
 
-    int numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
+    int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
                      13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
                      26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
                      39, 40, 41, 42, 43, 44, 45, 46, 47};
 
-    for (int i=0; i<numParts;i++){
+    for (int i = 0; i < numParts; i++) {
         start[i] = getStart(numbersSize, numParts, i);
         end[i] = getEnd(numbersSize, numParts, i);
     }
@@ -42,60 +46,64 @@ int main() {
     cl_command_queue commandQueue = clCreateCommandQueue(context, deviceId, NULL, NULL);
 
     cl_mem numbersMemBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, numbersSize * sizeof(int),
-                                       NULL, NULL);
+                                             NULL, NULL);
     cl_mem startMemBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, numParts * sizeof(int),
-                                             NULL, NULL);
+                                           NULL, NULL);
     cl_mem endMemBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY, numParts * sizeof(int),
-                                             NULL, NULL);
+                                         NULL, NULL);
     cl_mem resultsMemBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, numParts * sizeof(int),
-                                       NULL, NULL);
+                                             NULL, NULL);
 
-    clEnqueueWriteBuffer(commandQueue, numbersMemBuffer, CL_TRUE, 0, numbersSize*sizeof(int), numbers, 0,
+    clEnqueueWriteBuffer(commandQueue, numbersMemBuffer, CL_TRUE, 0, numbersSize * sizeof(int), numbers, 0,
                          NULL, NULL);
-    clEnqueueWriteBuffer(commandQueue, startMemBuffer, CL_TRUE, 0, numParts*sizeof(int), start, 0,
+    clEnqueueWriteBuffer(commandQueue, startMemBuffer, CL_TRUE, 0, numParts * sizeof(int), start, 0,
                          NULL, NULL);
-    clEnqueueWriteBuffer(commandQueue, endMemBuffer, CL_TRUE, 0, numParts*sizeof(int), end, 0,
+    clEnqueueWriteBuffer(commandQueue, endMemBuffer, CL_TRUE, 0, numParts * sizeof(int), end, 0,
                          NULL, NULL);
 
     size_t kernelSourceSize;
-    char* kernelSourceCode = getKernelCode("../vector_add.cl", &kernelSourceSize);
-    printf("Code:\n%s\n",kernelSourceCode);
+    char *kernelSourceCode = getKernelCode("../return_largest_prime.cl", &kernelSourceSize);
+    //printf("Code:\n%s\n",kernelSourceCode);
 
-    cl_program program = clCreateProgramWithSource(context, 1,(const char **) &kernelSourceCode,
-                                                   (const size_t*) &kernelSourceSize, NULL);
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **) &kernelSourceCode,
+                                                   (const size_t *) &kernelSourceSize, NULL);
 
     clBuildProgram(program, 1, &deviceId, NULL, NULL, NULL);
 
-    cl_kernel add_kernel = clCreateKernel(program, "vector_add", NULL);
+    cl_kernel add_kernel = clCreateKernel(program, "return_largest_prime", NULL);
 
-    int D = 10;
     clSetKernelArg(add_kernel, 0, sizeof(cl_mem), (void *) &numbersMemBuffer);
     clSetKernelArg(add_kernel, 1, sizeof(cl_mem), (void *) &startMemBuffer);
     clSetKernelArg(add_kernel, 2, sizeof(cl_mem), (void *) &endMemBuffer);
     clSetKernelArg(add_kernel, 3, sizeof(cl_mem), (void *) &resultsMemBuffer);
-    clSetKernelArg(add_kernel, 4, sizeof(int), (void *) &D);
 
-    size_t VECTOR_LENGTH2 = numParts;
-    clEnqueueNDRangeKernel(commandQueue, add_kernel, 1, NULL,  &VECTOR_LENGTH2, NULL,
+    size_t unsigned_numParts = numParts;
+    clEnqueueNDRangeKernel(commandQueue, add_kernel, 1, NULL, &unsigned_numParts, NULL,
                            0, NULL, NULL);
 
     clEnqueueReadBuffer(commandQueue, resultsMemBuffer, CL_TRUE, 0, numParts * sizeof(int),
                         results, 0, NULL, NULL);
 
 
-
-    for (int i=0; i<numParts;i++)
+/*
+    //Debug to see the result of each worker
+    for (int i=0; i<numParts;i++){
         printf("%d--> %d = %d\n",i, numbers[i], results[i]);
+    }
+*/
+
+    int max = largestNumber(results, numParts);
+    printf("The biggest prime number is: %d", max);
 
     return 0;
 }
 
-char* getKernelCode(const char* fileName, size_t *kernelSourceSize){
+char *getKernelCode(const char *fileName, size_t *kernelSourceSize) {
     FILE *kernelSourceFile;
     char *kernelSourceCode;
 
     kernelSourceFile = fopen(fileName, "r");
-    if(!kernelSourceFile){
+    if (!kernelSourceFile) {
         fprintf(stderr, "Failed to load kernel.\n");
         exit(1);
     }
@@ -115,7 +123,7 @@ int getStart(int numbersSize, int numParts, int index) {
 
     int start = index * partsSize;
 
-   return start;
+    return start;
 }
 
 int getEnd(int numbersSize, int numParts, int index) {
@@ -129,48 +137,6 @@ int getEnd(int numbersSize, int numParts, int index) {
     }
 
     return end;
-}
-
-int splitArray(int *parts, int numbers[], int numbersSize, int numParts, int index) {
-    int partsSize = numbersSize / numParts;
-
-    int start = index * partsSize;
-    int end = start + partsSize;
-
-    if (index + 1 == numParts) { // If its the last index add the rest of the numbers
-        end = numbersSize;
-    }
-
-    for (int i = start; i <= end; ++i) {
-        parts[i - start] = numbers[i];
-    }
-
-    return end - start;
-}
-
-int isPrime(int number) {
-    if (number == 0 || number == 1 || number == 4) { // Special cases
-        return 0;
-    }
-
-    for (int i = 2; i < number / 2; i++) { // General algorithm
-        if (number % i == 0)
-            return 0;
-    }
-
-    return 1;
-}
-
-int removeNonPrimes(int *primes, int numbers[], int numbersSize) {
-    int primesSize = 0;
-
-    for (int i = 0; i < numbersSize; ++i) {
-        if (isPrime(numbers[i])) {
-            primes[primesSize++] = numbers[i];
-        }
-    }
-
-    return primesSize;
 }
 
 int largestNumber(int arr[], int n) {
