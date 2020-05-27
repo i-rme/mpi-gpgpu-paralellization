1efdbe4cdb39677daa3c39403315433abfc1c8eb
i-rme <i-rme@users.noreply.github.com>
2020-05-27 13:08:57 +0200

    Uploaded beta project

---
+++ beta/PL4_GPGPU/cmake-build-debug/CMakeFiles/PL4_GPGPU.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/i-rme/Desktop/PL4_GPGPU/PL4_GPGPU/main.cpp" "C:/Users/i-rme/Desktop/PL4_GPGPU/PL4_GPGPU/cmake-build-debug/CMakeFiles/PL4_GPGPU.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
