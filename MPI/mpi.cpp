50dda279055dac50d4396bb0f889a022d5180bcf
i-rme <i-rme@users.noreply.github.com>
2020-06-02 00:26:58 +0200

    Added comments to MPI

--- MPI/mpi.cpp
+++ MPI/mpi.cpp
@@ -2,10 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 
-// Compile using    mpic++ -o mpi mpi.cpp
-// Run using        mpirun -n 6 -hosts mpi-1 ./mpi
+/*
+  Compile using    mpic++ -o mpi mpi.cpp
+  Run using        mpirun -n <numberOfWorkers> -hosts <listOfHosts> ./mpi
+  Example          mpirun -n 6 -hosts mpi-1 ./mpi
+*/
 
 int splitArray(int *parts, int numbers[], int numbersSize, int numParts, int index) {
+/*
+  splitArray(): Given an array, it gets a slice.
+  *parts: Output array, contains the slice
+  partsSize: Number of elements in the parts array
+  numbers[]: Input array, contains all numbers
+  numbersSize: Number of elements in the numbers array
+  numParts: Number of slices to divide the array
+  index: What slice we want
+*/
   int partsSize = numbersSize / numParts;
 
   int start = index * partsSize;
@@ -23,6 +35,10 @@ int splitArray(int *parts, int numbers[], int numbersSize, int numParts, int ind
 }
 
 int isPrime(int number) {
+/*
+  isPrime(): Returns 1 if prime, 0 if not
+  number: Input number
+*/
   if (number == 0 || number == 1 || number == 4) { // Special cases
     return 0;
   }
@@ -36,6 +52,12 @@ int isPrime(int number) {
 }
 
 int removeNonPrimes(int *primes, int numbers[], int numbersSize) {
+/*
+  removeNonPrimes(): Given an array, returns just the primes.
+  *primes: Output array, contains the primes
+  numbers[]: Input array, contains numbers
+  numbersSize: Number of elements in the numbers array
+*/
   int primesSize = 0;
 
   for (int i = 0; i < numbersSize; ++i) {
@@ -48,6 +70,11 @@ int removeNonPrimes(int *primes, int numbers[], int numbersSize) {
 }
 
 int largestNumber(int arr[], int n) {
+/*
+  largestNumber(): Given an array, returns the largest number.
+  arr[]: Input array, contains numbers
+  n: Number of elements in the numbers array
+*/
   int max = arr[0];
 
   for (int i = 1; i < n; i++) {
@@ -64,12 +91,21 @@ int largestNumber(int arr[], int n) {
 }
 
 void printArray(int array[], int arraySize) {
+/*
+  printArray(): Given an array, it prints its elements.
+  array[]: Input array, contains numbers
+  arraySize: Number of elements in the input array
+*/
   for (int i = 0; i < arraySize; i++) {
     printf("%i, ", array[i]);
   }
 }
 
 int main(int argc, char* argv[]) {
+/*
+  main(): Defines work, splits it, sends work to nodes, receives answer, outputs biggest answer.
+  numbers[]: Input array, contains numbers
+*/
 
   // INITIALIZE MPI
   int mpi_comm_size, mpi_comm_rank;
@@ -79,7 +115,7 @@ int main(int argc, char* argv[]) {
   MPI_Status status;
 
 
-  if(mpi_comm_rank == 0){
+  if(mpi_comm_rank == 0){ // If we are runnin on master
 
     // INPUT
     int numbers[] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12,
@@ -87,12 +123,10 @@ int main(int argc, char* argv[]) {
                        26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
                        39, 40, 41, 42, 43, 44, 45, 46, 47};
 
-    for (int i = 1; i < mpi_comm_size; i++) {
-      // VARIABLES
-      int index = i;    // What part we want
+    for (int i = 1; i < mpi_comm_size; i++) { //Preparing and sending the slices to the workers
 
+      int index = i;    // What slice we want
       int numbersSize = sizeof numbers / sizeof numbers[0];
-
       int numParts = mpi_comm_size;
 
       // OUTPUT
@@ -102,41 +136,39 @@ int main(int argc, char* argv[]) {
 
       // FUNCTION
       
-      int numeroSplit = splitArray(parts, numbers, numbersSize, numParts, index);
+      int numeroSplit = splitArray(parts, numbers, numbersSize, numParts, index); // Number of elements in the slice
 
-      MPI_Send(&numeroSplit, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-      MPI_Send(&parts, numeroSplit, MPI_INT, i, 1, MPI_COMM_WORLD);
+      MPI_Send(&numeroSplit, 1, MPI_INT, i, 1, MPI_COMM_WORLD);     //Sending the number of elements
+      MPI_Send(&parts, numeroSplit, MPI_INT, i, 1, MPI_COMM_WORLD); //Sending the slice
 
     }
 
-    // MASTER IS WORKING TOO
-
-    int index = 0;
+    // MASTER HAS TO WORK TOO
+    int index = 0;  // First slice
     int numbersSize = sizeof numbers / sizeof numbers[0];
     int numParts = mpi_comm_size;
     int partsSize = numbersSize / numParts;
     int parts[partsSize];
-    memset(parts, 0, sizeof parts);
-    int numeroSplit = splitArray(parts, numbers, numbersSize, numParts, index);
+    memset(parts, 0, sizeof parts);   // List of numbers gets initialized
+    int numeroSplit = splitArray(parts, numbers, numbersSize, numParts, index); // Number of elements in the slice
 
-    int primes[0];
+    int primes[0];  // List of primes gets initialized
     int primesSize = removeNonPrimes(primes, parts, numeroSplit);
-    int largest = largestNumber(primes, primesSize);
+    int largest = largestNumber(primes, primesSize);  
 
     // RECEIVING FROM SLAVES
-
-    int results[mpi_comm_size];
-    results[0] = largest;
+    int results[mpi_comm_size];    // Results holds the maximum prime from each worker
+    results[0] = largest;   // We store the result for master
 
     for (int i=1; i<mpi_comm_size; i++){
       int largest = 0;
       MPI_Recv(&largest, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
       //printf("Message received from %i: Largest is %i \n", i, largest);
-      results[i] = largest;
+      results[i] = largest;   // We store the result for each worker
     }
 
     // PRINT RESULT ARRAY
-    int absoluteLargest = largestNumber(results, mpi_comm_size);
+    int absoluteLargest = largestNumber(results, mpi_comm_size);   // We get the absolute maximum
 
     if(absoluteLargest == -1){
       printf("They were no primes in the numbers provided.");
@@ -146,24 +178,23 @@ int main(int argc, char* argv[]) {
 
 
 
-  }else{
+  }else{  // Here is where the workers do its thing
     
-    // RECEIVING WORK    
-    int numeroSplit = 0;
-    MPI_Recv(&numeroSplit, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-    int parts[numeroSplit];
-    MPI_Recv(&parts, numeroSplit, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    // RECEIVING WORK
+    int numeroSplit = 0;  // Initialization
+    MPI_Recv(&numeroSplit, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);  // Getting the array size from the master
+    int parts[numeroSplit];   // Innitialize array
+    MPI_Recv(&parts, numeroSplit, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);   // Receive slice
 
     // REMOVING NON PRIMES
-    int primes[0];
-    int primesSize = removeNonPrimes(primes, parts, numeroSplit);
+    int primes[0];   // Innitialize array
+    int primesSize = removeNonPrimes(primes, parts, numeroSplit);   // Get the number of primes found
 
     // SENDING LARGEST PRIME
-    int largest = largestNumber(primes, primesSize);
-    //printf("DEBUG: El worker %i hace send ", mpi_comm_rank);
-    MPI_Send(&largest, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-    //printf("DEBUG: El worker %i acaba de hacer send ", mpi_comm_rank);
-
+    int largest = largestNumber(primes, primesSize);   // Get largest prime
+    //printf("DEBUG: Worker %i is sending ", mpi_comm_rank);
+    MPI_Send(&largest, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);   // Send largest prime
+    //printf("DEBUG: Worker %i has finished its work ", mpi_comm_rank);
 
   }
 
