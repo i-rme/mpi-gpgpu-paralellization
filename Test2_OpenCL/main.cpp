18f5737848ab10b657b081e9eefaf961a3320126
Raúl Martínez (RME) <i-rme@users.noreply.github.com>
2020-06-04 17:37:55 +0200

    Added some big number array generator

--- Test2_OpenCL/main.cpp
+++ Test2_OpenCL/main.cpp
@@ -22,17 +22,22 @@ int main() {
     platformIndex = askPlatform();  // Nvidia, Intel, or AMD
     deviceIndex = askDevice();      // Device ID
 
-    const int numParts = 6;         // Number of parts to split our work into
+    /*
     const int numbersSize = 48;     // How many numbers we want to work with in total
+    int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+    */
+
+    const int numbersSize = 100000;     // How many numbers we want to work with in total
+    int numbers[numbersSize];
+    for (int n = 0; n < numbersSize; ++n) { numbers[n] = n; }   // Fill some numbers
+
+    //const int numParts = 6;         // Number of parts to split our work into
+    const int numParts = (numbersSize/5000)+1;         // Number of parts to split our work into
 
     int results[numParts];
     int start[numParts];
     int end[numParts];
-
-    int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-                     13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-                     26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-                     39, 40, 41, 42, 43, 44, 45, 46, 47};
 
     clock_t time_begin = clock();    //Start measuring execution time
 
